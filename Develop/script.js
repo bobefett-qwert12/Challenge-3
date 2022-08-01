a7af9d97d254c96f2bf55f26d489c6b5adf3f4b5
Ryan Perkins <perkins.ryanhanna@gmail.com>
2022-08-01 12:14:14 -0600

    Added a Javascript function to generate a random password based on user inputs

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var password = "";
+  var len = prompt("Input a password length between 8 and 128 characters.");
+  if(len > 128 || len < 8){
+    alert("Password length is outside given parameters. Please try again.");
+    return generatePassword();
+  };
+  var attributeArray = [];
+  alert("For the following prompts, enter 'Y' for yes, or 'N' for no.");
+  attributeArray.push(prompt("Do you want your password to contain lower case letters?").toUpperCase());
+  attributeArray.push(prompt("Do you want your password to contain upper case letters?").toUpperCase());
+  attributeArray.push(prompt("Do you want your password to contain numbers?").toUpperCase());
+  attributeArray.push(prompt("Do you want your password to contain special characters?").toUpperCase());
+  var characters = "";
+  if(attributeArray[0] === "Y"){
+    characters += "abcdefghijklmnopqrstuvwxyz";
+  };
+  if(attributeArray[1] === "Y"){
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  };
+  if(attributeArray[2] === "Y"){
+    characters += "1234567890";
+  };
+  if(attributeArray[3] === "Y"){
+    characters += "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+  };
+  if(attributeArray[0] !== "Y" &&
+  attributeArray[1] !== "Y" &&
+  attributeArray[2] !== "Y" &&
+  attributeArray[3] !== "Y"){
+    alert("Your password must contain one of the previously mentioned characters. Try again.");
+    return generatePassword();
+  };
+  for(var i=0; i<len; i++){
+    password += characters.charAt(Math.floor(Math.random()*(characters.length)));
+  }
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
